690a02e2519641bd39c18c53f94566b7350a2f3a
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-04-14 08:54:26 +0200

    Se crea la funcionalidad del boton, se muestra y cierra aún sin diseño

--- src/blackjack/presentation/user-modal-history/user-modal-record.js
+++ src/blackjack/presentation/user-modal-history/user-modal-record.js
@@ -1,6 +1,32 @@
 import modalHtml from './user-modal-record.html?raw';
 import './user-modal-record.css';
 
-export const modalRecord = (element) => {
+let modal;
 
+export const showModal = () => {
+    modal?.classList.remove('hide-modal');
+}
+
+export const hideModal = () => {
+    modal?.classList.add('hide-modal');
+}
+
+export const modalRecord = (element) => {
+    const button = document.querySelector('#btnRecord');
+
+    modal = document.createElement('div');
+    modal.innerHTML = modalHtml;
+    modal.className = 'modal-container hide-modal';
+
+    element.append(modal);
+
+    button.addEventListener('click', () =>{
+        showModal();
+    });
+
+    modal.addEventListener('click', (event) => {
+        if(event.target.className === 'modal-container' || event.target.className === 'span-x'){
+            hideModal();
+        }
+    });
 }
